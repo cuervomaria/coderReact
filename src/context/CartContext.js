d4ed800b6bd9d61abf3e5f54ac3d83c1d3bf6362
cuervomaria <cuervomaria@hotmail.com>
2021-02-01 13:06:20 -0300

    cambio de rama

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,58 +1,58 @@
 import React, { useState } from 'react'
 
-export const CartContext = React.createContext([])
+export const CartContext = React.createContext(0)
 
 export const CartContextProvider = ({ children }) => {
 
-    const [cart, setCart] = useState([])
+     const [cart, setCart] = useState(0)
 
 
-    const addItem = ({ producto }, cantidad) => {
-        let index = cart.findIndex(item => item.id === producto.id)
-        if (index !== -1) {
-            alert("El producto ya se encuentra agregado al carrito")
-        } else {
-            setCart([...cart, {
-                "id": producto.id,
-                "linea": producto.linea,
-                "varietal": producto.varietal,
-                "precio": producto.precio,
-                "cantidad": cantidad,
+    // const addItem = ({ producto }, cantidad) => {
+    //     let index = cart.findIndex(item => item.id === producto.id)
+    //     if (index !== -1) {
+    //         alert("El producto ya se encuentra agregado al carrito")
+    //     } else {
+    //         setCart([...cart, {
+    //             "id": producto.id,
+    //             "linea": producto.linea,
+    //             "varietal": producto.varietal,
+    //             "precio": producto.precio,
+    //             "cantidad": cantidad,
 
-            }])
-        }
-    }
+    //         }])
+    //     }
+    // }
 
 
 
-    const removeItem = (id) => {
-        let index = cart.findIndex(item => item.id === id)
-        if (index !== -1) {
-            setCart(cart.splice(index, 1))
-        }
+    // const removeItem = (id) => {
+    //     let index = cart.findIndex(item => item.id === id)
+    //     if (index !== -1) {
+    //         setCart(cart.splice(index, 1))
+    //     }
 
-    }
+    // }
 
-    const clear = () => {
-        setCart([])
-    }
+    // const clear = () => {
+    //     setCart([])
+    // }
 
-    const isInCart = (id) => {
-        let index = cart.findIndex(item => item.id === id)
-        let existe
-        if (index !== -1) {
-            existe = true
-        } else {
-            existe = false
-        }
-        console.log(existe)
-        return existe
-    }
+    // const isInCart = (id) => {
+    //     let index = cart.findIndex(item => item.id === id)
+    //     let existe
+    //     if (index !== -1) {
+    //         existe = true
+    //     } else {
+    //         existe = false
+    //     }
+    //     console.log(existe)
+    //     return existe
+    // }
 
 
 
     return (
-        <CartContext.Provider value={{cart, addItem, removeItem, clear, isInCart}}>
+        <CartContext.Provider value={[cart,setCart]}>
             {children}
         </CartContext.Provider>
     )
